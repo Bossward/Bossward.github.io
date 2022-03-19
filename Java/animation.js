3635b28484dd67c4b8996ba5830309c95ef4e38e
Bossward <100390013+Bossward@users.noreply.github.com>
2022-03-19 16:25:01 -0600

    Add files via upload

---
+++ Java/animation.js
@@ -0,0 +1,30 @@
+var butto = document.getElementById('button');
+document.getElementsByClassName("inner1")[0]
+
+butto.addEventListener('click', event => {
+ jls();
+ otha();
+});
+
+ function jls()
+     {
+
+    var elements = document.getElementsByClassName("inner");
+    for (var i = 0; i < elements.length; i++) {
+        var ne = elements[i].cloneNode(true);
+        elements[i].parentNode.replaceChild(ne, elements[i]);
+        elements[i].classList.remove("inner-a");
+        elements[i].classList.add("inner-a");
+    }
+ };
+
+ function otha()
+ {
+    var elements = document.getElementsByClassName("circleclass");
+    for (var i = 0; i < elements.length; i++) {
+        var ne = elements[i].cloneNode(true);
+        elements[i].parentNode.replaceChild(ne, elements[i]);
+        elements[i].classList.remove("circles-a");
+        elements[i].classList.add("circles-a");
+    }
+ };
